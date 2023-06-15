1fbe92e4b116fbe1306e90dc10213c08dc15ed64
pintoflager <jaakko0606@gmail.com>
2023-06-15 15:13:13 -0500

    update lua test

--- executor/lua/tests/run.lua
+++ executor/lua/tests/run.lua
@@ -1,9 +1,11 @@
 #!/usr/bin/env lua
 
--- # Print hello message
+-- Print hello message
 print("Test lua executor")
 
--- # Print boolean flag from command invokation
-print("Flag key / val")
-print(arg[1])
-print(arg[2])
+-- Print boolean flag from command invocation
+local key, val = arg[1], arg[2]
+print(string.format("Arg key: %s and val: %s", key, val))
+
+-- Print env key test
+print(string.format("Env value for key 'TESTER_LUA_FLAG': %s", os.getenv("TESTER_LUA_FLAG")))
