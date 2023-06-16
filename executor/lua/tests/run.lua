3e1d32985e3a8d51206d3ae679015bb84b3132f4
pintoflager <jaakko0606@gmail.com>
2023-06-16 14:57:33 -0500

    okay apps register and upgrade + run scripts locally and remotely

--- executor/lua/tests/run.lua
+++ executor/lua/tests/run.lua
@@ -1,14 +1,5 @@
 #!/usr/bin/env lua
 
--- -- # Print hello message
--- print("Test lua executor")
-
--- -- # Print boolean flag from command invocation
--- print("Flag key / val")
--- print(arg[1])
--- print(arg[2])
-
-
 -- Print hello message
 print("Test lua executor")
 
